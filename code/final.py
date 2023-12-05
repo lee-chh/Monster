8bb85709690acd8df2771c8a8d9d0df187401a68
lee-chh <113040880+lee-chh@users.noreply.github.com>
2023-12-05 15:14:54 +0900

    Update final.py

--- code/final.py
+++ code/final.py
@@ -1,4 +1,5 @@
 # 20201127 Lee Chang-hyun
+
 import pygame as pg
 import random
 from os import path
@@ -18,7 +19,7 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
 
-# initialize pg and create window
+# initialize pygame and create window
 pg.init()
 pg.mixer.init()
 screen = pg.display.set_mode((WIDTH, HEIGHT))
@@ -26,44 +27,20 @@ pg.display.set_caption("Be the Monster_20201127이창현")
 clock = pg.time.Clock()
 
 ball_images = []
-for i in range(1,10):
+for i in range(1, 10):
     filename = 'ball{}.png'.format(i)
-    img = pg.image.load(path.join(img_dir, filename)).convert()
-    
+    img = pg.image.load(path.join(img_dir, filename)).convert_alpha()
     ball_images.append(img)
 
 font_name = pg.font.match_font('arial')
+
+
 def draw_text(surf, text, size, x, y):
     font = pg.font.Font(font_name, size)
     text_surface = font.render(text, True, WHITE)
     text_rect = text_surface.get_rect()
     text_rect.midtop = (x, y)
     surf.blit(text_surface, text_rect)
-
-    def __init__(self, center, size):
-        pg.sprite.Sprite.__init__(self)
-        self.size = size
-        self.frame = 0
-        self.image = explosion_anim[self.size][self.frame]
-        self.rect = self.image.get_rect()
-        self.rect.center = center
-        
-        self.last_update = pg.time.get_ticks()
-        self.frame_rate = 75
-
-    def update(self):
-        now = pg.time.get_ticks()
-        if now - self.last_update > self.frame_rate:
-            self.last_update = now
-            self.frame += 1
-            # self.frame = (self.frame+1) % self.
-            if self.frame == len(explosion_anim[self.size]):
-                self.kill()
-            else:
-                center = self.rect.center
-                self.image = explosion_anim[self.size][self.frame]
-                self.rect = self.image.get_rect()
-                self.rect.center = center
 
 def show_go_screen():
     screen.fill(BLACK)
@@ -81,64 +58,33 @@ def show_go_screen():
             if event.type == pg.KEYUP:
                 waiting = False
 
+
 class Ball(pg.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, all_balls):
+        super().__init__(all_balls)
 
         # 랜덤한 이미지 설정
-        self.image = random.choice(ball_images)
-        self.rect = self.image.get_rect()
+        original_image = random.choice(ball_images)
 
         # 초기 위치 설정
-        self.rect.center = (random.randint(50, WIDTH - 50), 50)
+        self.rect = original_image.get_rect()
+        self.rect.x = WIDTH * 4 / 5
+        self.rect.y = HEIGHT * 1 / 5
+
+        # 이미지 크기 조정
+        scaled_width = 40  # 조절하고자 하는 폭
+        scaled_height = 40  # 조절하고자 하는 높이
+        self.image = pg.transform.scale(original_image, (scaled_width, scaled_height))
 
         # 초기 속도 및 가속도 설정
-        self.speed_x = random.choice([-5, 5])
-        self.speed_y = random.uniform(-5, 5)
+        self.speed_x = -3
+        self.speed_y = 0
         self.gravity = 0.5
+        self.x_acc = -1
 
         # 1초 간격으로 생성을 위한 타이머 설정
         self.appearance_timer = pg.time.get_ticks()
         self.appearance_interval = 1000  # 1 second
-
-
-    def update(self):
-        now = pg.time.get_ticks()
-        if now - self.appearance_timer > self.appearance_interval:
-            self.appearance_timer = now
-            new_ball = Ball(self.all_balls, self.all_equipment)
-            new_ball.speed_x = random.choice([-5, 5])
-            new_ball.speed_y = random.uniform(-5, 5)
-            new_ball.rect.center = (random.randint(50, WIDTH-50), 50)
-            self.all_balls.add(new_ball)
-
-        self.rect.x += self.speed_x
-        self.rect.y += self.speed_y
-        self.speed_y += self.gravity
-
-        if self.rect.bottom > HEIGHT:
-            self.kill()
-
-class Circle(pg.sprite.Sprite): 
-    def __init__(self, all_circles):
-        super().__init__(all_circles)
-
-        # 초기 위치 설정
-        self.rect = pg.Rect(1000,200, 20, 20)
-        self.rect.center = (1000,200)
-
-        # 초기 속도 및 가속도 설정
-        self.speed_x = 20
-        self.speed_y = 0
-        self.gravity = 0.5
-
-        # 랜덤한 이미지 설정
-        original_image = random.choice(ball_images)
-        
-        # 이미지 크기 조정
-        scaled_width = 20  # 조절하고자 하는 폭
-        scaled_height = 20  # 조절하고자 하는 높이
-        self.image = pg.transform.scale(original_image, (scaled_width, scaled_height))
-        self.rect = self.image.get_rect()
 
     def update(self):
         # 속도와 가속도의 영향을 받아 이동
@@ -147,25 +93,21 @@ class Circle(pg.sprite.Sprite):
 
         # 가속도 적용
         self.speed_y += self.gravity
+        self.speed_x += self.x_acc
 
-        # 특정 조건에 도달하면 제거
         if self.rect.y > HEIGHT:
-            print("Circle removed!")
-            self.kill() 
+            self.kill()
+
 
 # 스페이스바를 눌렀을 때의 플래그
 space_pressed = False
 
+# Ball 객체 생성
+all_balls = pg.sprite.Group()
+
 # Game loop
 game_over = True
 running = True
-
-# Circle 객체 생성
-all_circles = pg.sprite.Group()
-
-# Ball 그룹을 생성
-all_balls = pg.sprite.Group()
-
 while running:
     if game_over:
         show_go_screen()
@@ -180,24 +122,19 @@ while running:
         if event.type == pg.QUIT:
             running = False
 
-    # 스페이스바를 누르면 새로운 Circle 객체 생성
-    keys = pg.key.get_pressed()
-    if keys[pg.K_SPACE]:
-        print("Creating a new circle!")
-        Circle(all_circles)
-    # Circle 객체 업데이트
-    all_circles.update()
+    # 1초 간격으로 새로운 Ball 객체 생성
+    now = pg.time.get_ticks()
+    if all_balls and now - all_balls.sprites()[-1].appearance_timer > all_balls.sprites()[-1].appearance_interval:
+        Ball(all_balls)
+    elif not all_balls:
+        Ball(all_balls)
+
+    # Ball 객체 업데이트
+    all_balls.update()
 
     # 그리기
     screen.fill(BLACK)
-    all_circles.draw(screen)
-
-    # # Ball 객체 업데이트
-    # all_balls.update()
-
-    # # 그리기
-    # screen.fill(BLACK)
-    # all_balls.draw(screen)
+    all_balls.draw(screen)
 
     draw_text(screen, "run", 18, WIDTH / 2, HEIGHT / 2)
 
